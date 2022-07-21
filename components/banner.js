89ad68bf98d87ded7a04e314b31082a7d8fed43b
kimo2121 <kareem.gaad@gmail.com>
2022-07-21 11:36:31 +0200

    changed name

--- components/banner.js
+++ components/banner.js
@@ -5,10 +5,10 @@ const Banner = ({ buttonText, handleOnClick }) => {
   return (
     <div className={styles.container}>
       <h1 className={styles.title}>
-        <span className={styles.title1}>Coffee</span>
+        <span className={styles.title1}>Restaurant</span>
         <span className={styles.title2}>Connoisseur</span>
       </h1>
-      <p className={styles.subTitle}>Discover your local coffee shops!</p>
+      <p className={styles.subTitle}>Discover your local restaurant!</p>
       <button onClick={handleOnClick} className={styles.button}>
         {buttonText}
       </button>
